e6b5a0aeba034566939b108f7fbffc7677e65167
Alex Hipolito <alexheepo@gmail.com>
2021-06-19 17:35:15 +1000

    Adds required components via JS, changed default display css for Modal

---
+++ js/scripts.js
@@ -0,0 +1,58 @@
+/* Treehouse FSJS Techdegree
+ * Project 5 - Awesome Startup - Employee Directory
+ */
+
+/**
+* Search container
+*/
+const searchContainer = document.querySelector('.search-container');
+searchContainer.insertAdjacentHTML('beforeend', `
+    <form action="#" method="get">
+    <input type="search" id="search-input" class="search-input" placeholder="Search...">
+    <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
+    </form>
+`);
+
+/**
+* Gallery
+*/
+const gallery = document.getElementById('gallery');
+gallery.insertAdjacentHTML('beforeend', `
+    <div class="card">
+    <div class="card-img-container">
+        <img class="card-img" src="https://placehold.it/90x90" alt="profile picture">
+    </div>
+    <div class="card-info-container">
+        <h3 id="name" class="card-name cap">first last</h3>
+        <p class="card-text">email</p>
+        <p class="card-text cap">city, state</p>
+    </div>
+    </div>
+`);
+
+/**
+ * Modal
+ */
+document.querySelector('body').insertAdjacentHTML('beforeend', `
+    <div class="modal-container">
+    <div class="modal">
+        <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+        <div class="modal-info-container">
+            <img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">
+            <h3 id="name" class="modal-name cap">name</h3>
+            <p class="modal-text">email</p>
+            <p class="modal-text cap">city</p>
+            <hr>
+            <p class="modal-text">(555) 555-5555</p>
+            <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
+            <p class="modal-text">Birthday: 10/21/2015</p>
+        </div>
+    </div>
+
+    // IMPORTANT: Below is only for exceeds tasks 
+    <div class="modal-btn-container">
+        <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+        <button type="button" id="modal-next" class="modal-next btn">Next</button>
+    </div>
+    </div>
+`);
