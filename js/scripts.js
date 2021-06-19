78078a8421852a9e19ac972145048d59996b43c4
Alex Hipolito <alexheepo@gmail.com>
2021-06-19 23:02:56 +1000

    Generate modals for each employee

--- js/scripts.js
+++ js/scripts.js
@@ -8,14 +8,17 @@
  */
 // @todo: Catch unforseen errors
 function fetchData(url) {
-    return fetch(url)
-        .then(res => res.json())
-        .catch(error => console.log('Looks like there was a problem', error));
+  return fetch(url)
+    .then(res => res.json())
+    .catch(error => console.log('Looks like there was a problem', error));
 };
 
-fetch('https://randomuser.me/api/?results=12')
-    .then(response => response.json())
-    .then(data => generateEmployeeGallery(data));
+fetch('https://randomuser.me/api/?results=12&nat=us')
+  .then(response => response.json())
+  .then(data => {
+    generateEmployeeGallery(data);
+    generateEmployeeModal(data);
+  });
 
 /**
 * Search container
@@ -33,47 +36,52 @@ searchContainer.insertAdjacentHTML('beforeend', `
 */
 const gallery = document.getElementById('gallery');
 function generateEmployeeGallery(data) {
-    console.log(data);
-    data.results.map((employee) => {
-        gallery.insertAdjacentHTML('beforeend', `
-        <div class="card">
-            <div class="card-img-container">
-                <img class="card-img" src="${employee.picture.medium}" alt="Profile Picture of ${employee.name.first} ${employee.name.last}">
-            </div>
-            <div class="card-info-container">
-                <h3 id="name" class="card-name cap">${employee.name.first} ${employee.name.last}</h3>
-                <p class="card-text">${employee.email}</p>
-                <p class="card-text cap">${employee.location.city}, ${employee.location.state}</p>
-            </div>
-        </div>
-        `);
-    });
+  console.log(data);
+  data.results.map((employee) => {
+    gallery.insertAdjacentHTML('beforeend', `
+      <div class="card">
+          <div class="card-img-container">
+              <img class="card-img" src="${employee.picture.medium}" alt="Profile Picture of ${employee.name.first} ${employee.name.last}">
+          </div>
+          <div class="card-info-container">
+              <h3 id="name" class="card-name cap">${employee.name.first} ${employee.name.last}</h3>
+              <p class="card-text">${employee.email}</p>
+              <p class="card-text cap">${employee.location.city}, ${employee.location.state}</p>
+          </div>
+      </div>
+      `);
+  });
 }
 
 /**
  * Modal
  */
-document.querySelector('body').insertAdjacentHTML('beforeend', `
-    <div class="modal-container">
-    <div class="modal">
-        <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-        <div class="modal-info-container">
-            <img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">
-            <h3 id="name" class="modal-name cap">name</h3>
-            <p class="modal-text">email</p>
-            <p class="modal-text cap">city</p>
-            <hr>
-            <p class="modal-text">(555) 555-5555</p>
-            <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
-            <p class="modal-text">Birthday: 10/21/2015</p>
-        </div>
-    </div>
-
-    // IMPORTANT: Below is only for exceeds tasks 
-    <div class="modal-btn-container">
-        <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
-        <button type="button" id="modal-next" class="modal-next btn">Next</button>
-    </div>
-    </div>
-`);
+//@todo: Split cell number into proper formatting
+function generateEmployeeModal(data) {
+  data.results.map((employee) => {
+    document.querySelector('body').insertAdjacentHTML('beforeend', `
+      <div class="modal-container">
+          <div class="modal">
+              <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+              <div class="modal-info-container">
+                  <img class="modal-img" src="${employee.picture.large}" alt="Profile Picture of ${employee.name.first} ${employee.name.last}">
+                  <h3 id="name" class="modal-name cap">${employee.name.first} ${employee.name.last}</h3>
+                  <p class="modal-text">${employee.email}</p>
+                  <p class="modal-text cap">${employee.location.city}</p>
+                  <hr>
+                  <p class="modal-text">${employee.cell}</p>
+                  <p class="modal-text">${employee.location.street.number} ${employee.location.street.name}, ${employee.location.city}, ${employee.location.state} ${employee.location.postcode}</p>
+                  <p class="modal-text">Birthday: ${new Date(employee.dob.date).toLocaleDateString('en-US')}</p>
+              </div>
+          </div>
+  
+          // IMPORTANT: Below is only for exceeds tasks 
+          <div class="modal-btn-container">
+              <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+              <button type="button" id="modal-next" class="modal-next btn">Next</button>
+          </div>
+      </div>
+    `);
+  })
+}
 
